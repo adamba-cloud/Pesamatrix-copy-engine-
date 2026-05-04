a3e3a3a54d1112a0703915ad67d4e586aef3e481
adamba-cloud <philipcraig364@gmail.com>
2026-05-04 13:51:19 +0300

    Implement user management functions
    Added functions to manage users in the database.

--- models/users.py
+++ models/users.py
@@ -1,1 +1,20 @@
+from database import cursor, conn
 
+def add_user(telegram_id):
+    cursor.execute("""
+        INSERT OR IGNORE INTO users (telegram_id)
+        VALUES (?)
+    """, (telegram_id,))
+    conn.commit()
+
+
+def get_users():
+    cursor.execute("SELECT telegram_id, risk, active FROM users WHERE active=1")
+    return cursor.fetchall()
+
+
+def set_risk(telegram_id, risk):
+    cursor.execute("""
+        UPDATE users SET risk=? WHERE telegram_id=?
+    """, (risk, telegram_id))
+    conn.commit()
