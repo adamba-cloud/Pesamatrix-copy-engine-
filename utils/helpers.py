4b658123734b88f0cfb2b1415405889441f7b63f
adamba-cloud <philipcraig364@gmail.com>
2026-05-04 13:54:07 +0300

    Update helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,1 +1,15 @@
+from datetime import datetime
 
+def now():
+    return str(datetime.now())
+
+
+def format_trade(trade):
+    return f"""
+PAIR: {trade['symbol']}
+SIDE: {trade['side']}
+ENTRY: {trade['entry']}
+SL: {trade['sl']}
+TP: {trade['tp']}
+TIME: {trade.get('timestamp', now())}
+"""
